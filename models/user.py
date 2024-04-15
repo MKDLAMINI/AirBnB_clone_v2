a13cfb3556b87057bb4485ba628bd44ee6ff63f8
AerinLom <aerinlom1@gmail.com>
2024-04-15 14:12:25 -0700

    Updated User

--- models/user.py
+++ models/user.py
@@ -1,11 +1,14 @@
 #!/usr/bin/python3
 """This module defines a class User"""
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String
 
 
-class User(BaseModel):
-    """This class defines a user by various attributes"""
-    email = ''
-    password = ''
-    first_name = ''
-    last_name = ''
+class User(BaseModel, Base):
+    """ User class that inherits from BaseModel and Base """
+    __tablename__ = 'users'
+
+    password = Column(String(128), nullable=False)
+    email = Column(String(128), nullable=False)
+    last_name = Column(String(128))
+    first_name = Column(String(128))
