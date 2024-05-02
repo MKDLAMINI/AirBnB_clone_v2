4367689582f86baca21c2f9c9cf1c9ebf3778f54
AerinLom <aerinlom1@gmail.com>
2024-05-02 12:42:12 -0700

    Setup using puppet

--- 101-setup_web_static.pp
+++ 101-setup_web_static.pp
@@ -1,56 +1,88 @@
 # This script sets up web servers for web_static deployment using puppet
 
+$nginx_conf = "server {
+    listen 80 default_server;
+    listen [::]:80 default_server;
+    add_header X-Served-By ${hostname};
+    root   /var/www/html;
+    index  index.html index.htm;
+    location /hbnb_static {
+        alias /data/web_static/current;
+        index index.html index.htm;
+    }
+    location /redirect_me {
+        return 301 https://th3-gr00t.tk;
+    }
+    error_page 404 /404.html;
+    location /404 {
+      root /var/www/html;
+      internal;
+    }
+}"
+
 # Install nginx package
 package { 'nginx':
   ensure   => 'present',
   provider => 'apt'
 }
 
-# Allow HTTP traffic in firewall
-firewall { 'Nginx HTTP':
-  port   => 80,
-  proto  => 'tcp',
-  action => 'accept',
-}
-
 # Create directories and index.html file
+file { '/data':
+  ensure  => 'directory'
+} ->
+
+file { '/data/web_static':
+  ensure => 'directory'
+} ->
+
+file { '/data/web_static/releases':
+  ensure => 'directory'
+} ->
+
 file { '/data/web_static/releases/test':
-  ensure => directory,
-  recurse => true,
-}
+  ensure => 'directory'
+} ->
 
 file { '/data/web_static/shared':
-  ensure => directory,
-}
+  ensure => 'directory'
+} ->
 
 file { '/data/web_static/releases/test/index.html':
-  ensure  => file,
-  content => "<html>\n  <head>\n  </head>\n  <body>\n    Holberton School\n  </body>\n</html>\n",
-}
+  ensure  => 'present',
+  content => "Holberton School\n"
+} ->
 
-# Create symbolic link
 file { '/data/web_static/current':
-  ensure => link,
-  target => '/data/web_static/releases/test',
-}
+  ensure => 'link',
+  target => '/data/web_static/releases/test'
+} ->
 
-# Change ownership of directories
 exec { 'chown -R ubuntu:ubuntu /data/':
-  command => '/bin/chown -R ubuntu:ubuntu /data/',
-  path    => ['/bin', '/usr/bin'],
-  onlyif  => '/bin/test ! -e /data/web_static/releases/test/index.html',
+  path => '/usr/bin/:/usr/local/bin/:/bin/'
 }
 
-# Configure nginx
-file_line { 'nginx_hbnb_static_alias':
-  path    => '/etc/nginx/sites-enabled/default',
-  line    => '        location /hbnb_static { alias /data/web_static/current/; }',
-  match   => 'listen 80 default_server',
-  after   => true,
-  notify  => Service['nginx'],
-}
+file { '/var/www':
+  ensure => 'directory'
+} ->
 
-service { 'nginx':
-  ensure => running,
-  enable => true,
+file { '/var/www/html':
+  ensure => 'directory'
+} ->
+
+file { '/var/www/html/index.html':
+  ensure  => 'present',
+  content => "Holberton School\n"
+} ->
+
+file { '/var/www/html/404.html':
+  ensure  => 'present',
+  content => "Ceci n'est pas une page\n"
+} ->
+
+file { '/etc/nginx/sites-available/default':
+  ensure  => 'present',
+  content => $nginx_conf
+
+exec { 'nginx restart':
+  path => '/etc/init.d/'
 }
